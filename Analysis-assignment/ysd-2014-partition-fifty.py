6482f01d9cd45d781a3583af961759469f7d7659
YatiDole <ydole@hawk.iit.edu>
2020-05-10 22:18:34 -0500

    deleted:    Analysis-assignment/Readme.md 	deleted:    Analysis-assignment/images/2014-min-max.JPG 	deleted:    Analysis-assignment/images/hdfs file system.JPG 	deleted:    Analysis-assignment/images/min-max-analysis.JPG 	deleted:    Analysis-assignment/images/partition-analysis.JPG 	deleted:    Analysis-assignment/images/read-write-csv.JPG 	deleted:    Analysis-assignment/images/valid-record-count-press.JPG 	deleted:    Analysis-assignment/images/valid-record-count-temp.JPG 	deleted:    Analysis-assignment/images/valid-record-press.JPG 	deleted:    Analysis-assignment/images/ysd-20-partition-analysis.JPG 	deleted:    Analysis-assignment/ysd-20-min-max-airtemp.py 	deleted:    Analysis-assignment/ysd-2014-lz4-json.py 	deleted:    Analysis-assignment/ysd-2014-lz4.py 	deleted:    Analysis-assignment/ysd-2014-min-max-airtemp.py 	deleted:    Analysis-assignment/ysd-2014-partition-fifty.py 	deleted:    Analysis-assignment/ysd-2014-partition-one.py 	deleted:    Analysis-assignment/ysd-2014-partition-two-hundred.py 	deleted:    Analysis-assignment/ysd-2014-partitionby.py 	deleted:    Analysis-assignment/ysd-2014-records-count-temp.py 	deleted:    Analysis-assignment/ysd-2014-valid-records-count-press.py 	deleted:    Analysis-assignment/ysd-2014-valid-records-press.py 	deleted:    Analysis-assignment/ysd-2014-valid-records-temp.py 	deleted:    RW-assignment/Readme.md 	deleted:    RW-assignment/ysd-2014-read-write-csv.py 	deleted:    RW-assignment/ysd-2014-read-write-parquet.py 	new file:   ydole

--- Analysis-assignment/ysd-2014-partition-fifty.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("Demo Spark Python Cluster Program").getOrCreate()
-
-df = spark.read.option("header", "true").option("inferSchema", "true").csv("hdfs://namenode/output/itmd-521/ysd/2014/csv-file")
-
-df.repartition(50).write.format("csv").option("compression","lz4").mode("overwrite").option("header","true").save("hdfs://namenode/output/itmd-521/ysd/2014/repartition/50")
--- Analysis-assignment/ysd-2014-partition-one.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("Demo Spark Python Cluster Program").getOrCreate()
-
-df = spark.read.option("header", "true").option("inferSchema", "true").csv("hdfs://namenode/output/itmd-521/ysd/2014/csv-file")
-
-# page 82 Spark definetive guide -compression lz4
-df.repartition(1).write.format("csv").option("compression","lz4").mode("overwrite").option("header","true").save("hdfs://namenode/output/itmd-521/ysd/2014/repartition/1")
